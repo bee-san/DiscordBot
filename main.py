ed06d57034d2d8a68411a4d26f9b124a18a96196
Jay001 <minkyman12@gmail.com>
2021-02-10 21:59:59 +0000

    Added discord bot

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from name_that_hash import runner
+import json
+from search_that_hash import api
+import random
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+@bot.command()
+async def hash(ctx, hash):
+    result = json.loads(api.return_as_fast_json([hash]))
+    embed = discord.Embed(title="Hashy - Cracks hashes via Search-That-Hash API", description=hash, color=random.randint(0,16777215)) #,color=Hex code
+    try:
+        embed.add_field(name="Plaintext", value=result[0][hash], inline=False)
+    except:
+        embed.add_field(name="Plaintext", value="Could not crack hash", inline=False)
+    embed.set_footer(text="https://github.com/HashPals/Search-That-Hash") #if you like to
+    await ctx.send(embed=embed)
+
+bot.run("MzM0NzExNTYzMzI1ODAwNDU5.WWY6bw.20Ta-ohVLf6n7wMbFR6XmocopNU")
