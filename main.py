ee7afc10625090739c020f7f6e7aa98df10c9f1a
Jayy001 <56607897+Jayy001@users.noreply.github.com>
2021-02-15 19:16:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ async def hash(ctx, hash):
     result = json.loads(api.return_as_fast_json([hash]))
     embed = discord.Embed(title="Hashy - Cracks hashes via Search-That-Hash API", description=hash, color=random.randint(0,16777215)) #,color=Hex code
     try:
-        embed.add_field(name="Plaintext", value=result[0][hash], inline=False)
+        if result[0][hash][0] == {}:
+            embed.add_field(name="Plaintext", value="Could not crack hash", inline=False)
+        else:
+            embed.add_field(name="Plaintext", value=result[0][hash], inline=False)
     except:
         embed.add_field(name="Plaintext", value="Could not crack hash", inline=False)
     embed.set_footer(text="https://github.com/HashPals/Search-That-Hash") #if you like to
